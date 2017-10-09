5103b66de37c7f7c58d834b182cf1d9c5e7ff1c3
Nic Acton <awesome.acton@gmail.com>
2017-10-09 01:37:27 -0400

    second commit

---
+++ utils/zillow_helpers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import operator
+import sys
+import os
+sys.path.insert(0,'../')
+
+def txt_to_df(text_file_path):
+    '''
+    Takes a file path to a text file that follows the syntax of pipe-delimited values and converts it to a good dataframe.
+    '''
+    df = pd.read_csv(text_file_path, sep="|", header=None, low_memory=False)
+    return df
+
+def txt_to_pickle(text_file_path, filename):
+    '''
+    Takes a file path to a text file that follows the syntax of pipe-delimited values and converts it to a good dataframe.
+    '''
+    df = txt_to_df(text_file_path)
+    df.to_pickle(filename)
