4dae4b1b06ac916906843918aa157468753d0dac
Nic Acton <awesome.acton@gmail.com>
2017-10-15 20:36:40 -0400

    massive_commit
    Added tutorial for usage of the data-set

--- utils/zillow_helpers.py
+++ utils/zillow_helpers.py
@@ -17,3 +17,19 @@ def txt_to_pickle(text_file_path, filename):
     '''
     df = txt_to_df(text_file_path)
     df.to_pickle(filename)
+    
+def txt_to_column_df(data_file_path, clist_file_path):
+    '''
+    Takes a string file-path to a text file that follows the syntax of pipe-delimited values, 
+    and a string file-path to a list of their column names and converts it to a column-named dataframe
+    for useful work!
+    '''
+    main_df = pd.read_csv(data_file_path, sep="|", header=None, low_memory=False)
+    
+    cHelp = open(clist_file_path, 'r')
+    cHelpArray = []
+    for line in cHelp:
+        cHelpArray.append(line.rstrip())
+    
+    main_df.columns=cHelpArray
+    return main_df
